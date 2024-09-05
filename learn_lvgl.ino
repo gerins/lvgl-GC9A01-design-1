372af6a7c928793dc7633ab867bfecfb45f6cbcc
Garin <gerin.rin@gmail.com>
2024-09-05 14:46:54 +0700

    initial commit

--- learn_lvgl.ino
+++ learn_lvgl.ino
@@ -45,50 +45,68 @@ void setup() {
     lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0xFF000000), LV_PART_MAIN);  // Set background color to black
 
     //================================== Start Code ==================================
-    // Temp ring
-    int tempValue = 50;
-    lv_obj_t *tempArc = CreateBaseArc(0xFFD72749, 230, 0, 100, 100, 170);
-    lv_arc_set_value(tempArc, tempValue);
-    lv_obj_remove_style(tempArc, NULL, LV_PART_KNOB);                             // Remove knob
-    lv_obj_set_style_arc_color(tempArc, lv_color_hex(0xFFD72749), LV_PART_MAIN);  // Main color
-    lv_obj_t *tempSymbol = lv_label_create(lv_scr_act());                         //
-    lv_obj_align(tempSymbol, LV_ALIGN_CENTER, 0, 106);                            // Center position
-    lv_obj_set_style_text_font(tempSymbol, &custom_symbol_16, 0);                 // Change font style
-    lv_obj_set_style_text_color(tempSymbol, lv_color_hex(0xFFD72749), 0);         // Change text color
-    lv_label_set_text(tempSymbol, tempLogo);                                      // Set logo symbol
 
-    // Volt ring
-    lv_obj_t *voltArc = CreateBaseArc(0xFFD89843, 230, 0, 15, 190, 260);
-    lv_arc_set_value(voltArc, 12);
-    lv_obj_remove_style(voltArc, NULL, LV_PART_KNOB);                      // Remove knob
+    //================================== Volt Arc ==================================
+    lv_obj_t *voltArc = CreateBaseArc(0xFFD89843, 230, 0, 16, 190, 260);
+    int voltValue = 13;
+    lv_arc_set_value(voltArc, voltValue);
+    // Knob configuration
+    lv_obj_remove_style(voltArc, NULL, LV_PART_KNOB);  // Remove knob
+    // Symbol volt
     lv_obj_t *voltSymbol = lv_label_create(lv_scr_act());                  //
     lv_obj_align(voltSymbol, LV_ALIGN_CENTER, -108, 0);                    // Center position
     lv_obj_set_style_text_font(voltSymbol, &custom_symbol_16, 0);          // Change font style
     lv_obj_set_style_text_color(voltSymbol, lv_color_hex(0xFFD89843), 0);  // Change text color
     lv_label_set_text(voltSymbol, lightingBoltLogo);                       // Set logo symbol EF 83 A7
+    // Volt Value
+    lv_obj_t *labelVolt = lv_label_create(lv_scr_act());
+    lv_label_set_text_fmt(labelVolt, "%s", "12");
+    lv_obj_set_style_text_color(labelVolt, lv_color_hex(0xFFFFFFFF), 0);  // Change text color
+    lv_arc_rotate_obj_to_angle(voltArc, labelVolt, -17);
+
+    //================================== Temperature Arc ==================================
+    lv_obj_t *tempArc = CreateBaseArc(0xFFD72749, 230, 0, 100, 280, 350);  // Indicator color
+    // lv_obj_set_style_arc_color(tempArc, lv_color_hex(0xFFD72749), LV_PART_MAIN);  // Bucket color
+    int tempValue = 50;
+    lv_arc_set_value(tempArc, tempValue);
+    // Knob configuration
+    lv_obj_remove_style(tempArc, NULL, LV_PART_KNOB);                            // Remove knob
+    lv_obj_set_style_bg_color(tempArc, lv_color_hex(0xFFFFFFFF), LV_PART_KNOB);  // Knob color
+    lv_obj_set_style_pad_all(tempArc, 1, LV_PART_KNOB);                          // Set 5px padding around knob
+    // Symbol
+    lv_obj_t *tempSymbol = lv_label_create(lv_scr_act());                  //
+    lv_obj_align(tempSymbol, LV_ALIGN_CENTER, 0, -108);                    // Center position
+    lv_obj_set_style_text_font(tempSymbol, &custom_symbol_16, 0);          // Change font style
+    lv_obj_set_style_text_color(tempSymbol, lv_color_hex(0xFFD72749), 0);  // Change text color
+    lv_label_set_text(tempSymbol, tempLogo);                               // Set logo symbol
+    // Temp Value
+    lv_obj_t *labelTemp = lv_label_create(lv_scr_act());
+    lv_label_set_text_fmt(labelTemp, "%d", 50);
+    lv_obj_set_style_text_color(labelTemp, lv_color_hex(0xFFFFFFFF), 0);  // Change text color
+    lv_arc_rotate_obj_to_angle(tempArc, labelTemp, -17);
 
     // Arc 3
-    lv_obj_t *arc3 = CreateBaseArc(0xFF18BB8D, 230, 0, 100, 280, 350);
-    lv_arc_set_value(arc3, 100);
-    lv_obj_remove_style(arc3, NULL, LV_PART_KNOB);                             // Remove knob
-    lv_obj_t *mountainSymbol = lv_label_create(lv_scr_act());                  //
-    lv_obj_align(mountainSymbol, LV_ALIGN_CENTER, 0, -108);                    // Center position
-    lv_obj_set_style_text_font(mountainSymbol, &custom_symbol_16, 0);          // Change font style
-    lv_obj_set_style_text_color(mountainSymbol, lv_color_hex(0xFF18BB8D), 0);  // Change text color
-    lv_label_set_text(mountainSymbol, mountainLogo);                           // Set logo symbol EF 83 A7
-
-    // Arc 4
-    lv_obj_t *arc4 = CreateBaseArc(0xFF3654F5, 230, 0, 100, 10, 80);
+    lv_obj_t *arc4 = CreateBaseArc(0xFF0AC94C, 230, 0, 100, 10, 80);
     lv_arc_set_value(arc4, 100);
     lv_obj_remove_style(arc4, NULL, LV_PART_KNOB);                              // Remove knob
     lv_obj_t *cloudMoonSymbol = lv_label_create(lv_scr_act());                  //
     lv_obj_align(cloudMoonSymbol, LV_ALIGN_CENTER, 108, 0);                     // Center position
     lv_obj_set_style_text_font(cloudMoonSymbol, &custom_symbol_16, 0);          // Change font style
-    lv_obj_set_style_text_color(cloudMoonSymbol, lv_color_hex(0xFF3654F5), 0);  // Change text color
+    lv_obj_set_style_text_color(cloudMoonSymbol, lv_color_hex(0xFF0AC94C), 0);  // Change text color
     lv_label_set_text(cloudMoonSymbol, cloudMoonLogo);                          // Set logo symbol EF 83 A7
 
+    // Arc 4
+    lv_obj_t *arc3 = CreateBaseArc(0xFF18BB8D, 230, 0, 100, 100, 170);
+    lv_arc_set_value(arc3, 100);
+    lv_obj_remove_style(arc3, NULL, LV_PART_KNOB);                             // Remove knob
+    lv_obj_t *mountainSymbol = lv_label_create(lv_scr_act());                  //
+    lv_obj_align(mountainSymbol, LV_ALIGN_CENTER, 0, 106);                     // Center position
+    lv_obj_set_style_text_font(mountainSymbol, &custom_symbol_16, 0);          // Change font style
+    lv_obj_set_style_text_color(mountainSymbol, lv_color_hex(0xFF18BB8D), 0);  // Change text color
+    lv_label_set_text(mountainSymbol, mountainLogo);                           // Set logo symbol EF 83 A7
+
     // RPM Label middle
-    rpmLabel = lv_label_create(lv_scr_act());
+    lv_obj_t *rpmLabel = lv_label_create(lv_scr_act());
     lv_obj_align(rpmLabel, LV_ALIGN_CENTER, 0, 0);                       // Center position
     lv_obj_set_style_text_font(rpmLabel, &digital_font_50, 0);           // Change font style
     lv_obj_set_style_text_color(rpmLabel, lv_color_hex(0xFFFFFFFF), 0);  // Change text color
